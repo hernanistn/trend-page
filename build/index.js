9e160bdad13b93a7d6ea4fcbfe5e25353c7b784a
Hernani Steinheuser <hernani.boaski@premiersoft.net>
2022-09-09 14:20:55 -0300

    build folder

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Server_1 = __importDefault(require("./express/Server"));
+var server = new Server_1.default();
+server.initialize();
