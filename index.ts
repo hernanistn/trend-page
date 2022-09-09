81f2dc5dd9641f6be1d528ccae069aa3e1f033cf
Hernani Steinheuser <hernani.boaski@premiersoft.net>
2022-09-09 17:51:01 -0300

    vercel config 6

--- index.ts
+++ index.ts
@@ -2,19 +2,14 @@
 import express from "express"
 
 const app = express()
-const router = express.Router()
-router.get("/auth", (req, res) => {
+app.get("/auth", (req, res) => {
     res.send("code: " + req.query.code)
 })
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-
-
-
-app.use(`/.netlify/functions/api`, router)
 app.listen(3001, () => {
     console.log("Running on port 5000.")
 })
